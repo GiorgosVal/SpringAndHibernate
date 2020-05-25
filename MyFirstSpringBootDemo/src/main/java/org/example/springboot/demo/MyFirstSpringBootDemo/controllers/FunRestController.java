ac550e1f3d4320a31a385a6c0506976447d31da1
Giorgos Valamatsas <giorgos.valamats@gmail.com>
2020-05-25 21:57:57 +0300

    Spring Boot basic application properties

--- MyFirstSpringBootDemo/src/main/java/org/example/springboot/demo/MyFirstSpringBootDemo/controllers/FunRestController.java
+++ MyFirstSpringBootDemo/src/main/java/org/example/springboot/demo/MyFirstSpringBootDemo/controllers/FunRestController.java
@@ -1,5 +1,6 @@
 package org.example.springboot.demo.MyFirstSpringBootDemo.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,13 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+
+
+    @Value("${custom.prop.name}")
+    private String name;
+
+    @Value("${custom.prop.height}")
+    private String height;
 
     @GetMapping("/")
     public String sayHello() {
@@ -16,6 +24,11 @@ public class FunRestController {
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Run a hard 5k!";
+    }
+
+    @GetMapping("/athleteInfo")
+    public String getAthletesInfo(){
+        return "Athlete: " + name + ", height: " + height + "cm.";
     }
 
 
